6a46bf65ece893fe0ef973a392e6033cda7de267
Thummarong <thummarong22@hotmail.com>
2018-04-01 16:14:49 +0700

    ber

--- co-project/src/co/project/CoProject.java
+++ co-project/src/co/project/CoProject.java
@@ -16,6 +16,13 @@ public class CoProject {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+ 
     
+    }
+        public static void add(int num1 , int num2){
+        num1 = 5;
+        num2 = 6;
+       int result =  num1 + num2;
+       System.out.print("result is: "+result);
+        }
 }
