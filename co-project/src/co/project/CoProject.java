840ac62b37834fafc6418973f12aeeee6f12894a
Game <scropion05@hotmail.com>
2018-04-01 16:14:49 +0700

    Game

--- co-project/src/co/project/CoProject.java
+++ co-project/src/co/project/CoProject.java
@@ -1,21 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package co.project;
 
-/**
- *
- * @author STD
- */
+import java.util.Scanner;
+
 public class CoProject {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        int a,b;
+        int sum=0;
+        
+        Scanner a1 = new Scanner(System.in);
+        System.out.print("Enter Number1: ");
+        a = a1.nextInt();
+        
+        Scanner b1 = new Scanner(System.in);
+        System.out.print("Enter Number2: ");
+        b = b1.nextInt();
+                
+        
+        sum=a+b;
+        
+         System.out.println("Sum is : "+sum);
+                
+        
+       
     }
     
 }
